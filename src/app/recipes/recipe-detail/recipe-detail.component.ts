c757d6babc3465d00e2326b48cf1a79aa78eff00
Tim Farmer <tfarmer4@gmail.com>
2018-01-24 20:14:07 -0600

    adding routing to multiple components and adding a new recipe-edit component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Recipe} from "../recipe.model";
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 import {ShoppingListService} from '../../shopping-list/shopping-list.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,14 +10,26 @@ import {ShoppingListService} from '../../shopping-list/shopping-list.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-  constructor(private shoppingListService: ShoppingListService) { }
+  recipe: Recipe;
+  id: number;
+  constructor(private shoppingListService: ShoppingListService,
+              private route: ActivatedRoute,
+              private recipeService: RecipeService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   sendToShoppingList() {
     this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
